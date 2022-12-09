4d83cf9d7dc12bde35e1c5ffa9fec92cb627ca28
user1 <user1@DESKTOP-MPUKNTA>
2022-12-09 17:45:40 +0900

    ADD : day13 Files
    File 클래스, 네트워크 프로그래밍(Socket Programming) - 채팅 / 숫자야구

---
+++ src/com/kh/day13/socket/baseball/BaseballServer.java
@@ -0,0 +1,66 @@
+package com.kh.day13.socket.baseball;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class BaseballServer {
+	public static void main(String [] args) {
+		ServerSocket serverSocket = null;
+		int port = 8589;
+		InputStream is = null;
+		OutputStream os = null;
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		int [] numbers = new int[3];
+		try {
+			System.out.println("서버소켓을 생성하였습니다.");
+			serverSocket = new ServerSocket(port);
+			Thread.sleep(2000);
+			System.out.println("클라이언트의 접속을 기다립니다.");
+			Socket socket = serverSocket.accept();
+			System.out.println("클라이언트가 접속했습니다.");
+			is = socket.getInputStream();
+			os = socket.getOutputStream();
+			dis = new DataInputStream(is);
+			dos = new DataOutputStream(os);
+			Random rand = new Random();
+			// 번호 3개를 뽑은 후 게임 준비해야함
+			for(int i = 0; i < numbers.length; i++) {
+				numbers[i] = rand.nextInt(9)+1;
+				// 중복제거
+				for(int e = 0; e< i; e++) {
+					if(numbers[e] == numbers[i]) {
+						i--;
+						break;
+					}
+				}
+			}
+			System.out.println("서버 숫자 → " + numbers[0] + " " + numbers[1] + " " + numbers[2] + " ");
+			System.out.println("서버 준비 완료");
+			// 값 받기
+			String readNum = dis.readUTF();
+			System.out.println("받기 : " + readNum);
+			/* 받은 값과 numbers의 값을 비교했을 때
+			 * 숫자가 맞고 위치도 맞는지, 숫자는 맞는데 위치는 틀린지,
+			 * 아무것도 맞지 않았는지를 스크라이크/볼로 출력해준다.
+			 */
+			int strike = 0;
+			int ball = 0;
+			
+			System.out.println(strike + " 스트라이크 / " + ball + " 볼");
+			// 클라이언트로 결과값 보내주기!
+			
+			// 스트라이크가 3이면 게임종료하기
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
